a1ecba162fe8bef22ce2b3ebad469c0880230e34
ntt-vulk <vu.lk@nttnetworks.com>
2020-11-17 09:48:34 +0700

    basic connect database

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ Start
 - npm init -y : Generate file package json
 - npm install express nodemon: express => create server; nodemon => run project continuously
 - npm install mongoose : install database mongoose;
+Database
+Register database
+mlab: https://cloud.mongodb.com/
+tenlavodich@gmail.com/Thiẻn92
+
+- npm install dotenv: call specific data in file .env
--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const app = express();
-
+const mongoose = require('mongoose');
+require('dotenv/config');
 //Middlewares
 app.use('/posts', () =>{
 	console.log('The middlewre running');
@@ -13,5 +14,14 @@ app.get('/', (req, res) => {
 	res.send('We are on home');
 })
 
+
+//connect to DB
+
+mongoose.connect(
+ process.env.DB_CONNECTION,
+ { useNewUrlParser: true,  useUnifiedTopology: true},
+ ()=>{
+	console.log('connected to DB');
+});
 //Listening to the sever
 app.listen(3000);
